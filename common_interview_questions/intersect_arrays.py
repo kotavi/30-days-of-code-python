74e11c8ab97f74a65161c85d2725422e593559f4
Tetiana Korchak <korchak.tetiana@gmail.com>
2020-02-07 12:22:06 -0600

    test commit

--- common_interview_questions/intersect_arrays.py
+++ common_interview_questions/intersect_arrays.py
@@ -8,22 +8,27 @@ Output: [2,2]
 """
 from collections import Counter
 
+
 def intersect_1(nums1, nums2):
-        lst = []
-        for value in nums1:
-            if value in nums2:
-                lst.append(value)
-                nums2.remove(value)
-        return lst
+    lst = []
+    for value in nums1:
+        if value in nums2:
+            lst.append(value)
+            nums2.remove(value)
+        if not nums2:
+            return lst
+    return lst
+
 
 def intersect_2(nums1, nums2):
     c1 = Counter(nums1)
     c2 = Counter(nums2)
     lst = []
-    for value in c1:
-        if value in c2:
-            lst += [value] * min(c1[value], c2[value])
+    for key in c1:
+        if key in c2:
+            lst += [key] * min(c1[key], c2[key])
     return lst
 
-print(intersect_1([1,1,2,2,3], [1,2,3,3]))
-print(intersect_2([1,1,2,2,3], [1,2,3,3]))
+
+print(intersect_1([1, 1, 2, 2, 3], [1, 2, 3, 3]))
+print(intersect_2([1, 1, 2, 2, 3], [1, 2, 3, 3]))
--- w3resource_strings/remove_all_consecutive_duplicates.py
+++ w3resource_strings/remove_all_consecutive_duplicates.py
@@ -18,3 +18,15 @@ print(result)
 
 result = "".join(i for i, _ in itertools.groupby(str1))
 print(result)
+
+str1 = "xxxxyyyyyyxxxxxxyyyyyyyy"
+lst = list(str1)
+prev = str1[0]
+i = 1
+while i < len(lst):
+    if prev == lst[i]:
+        del lst[i]
+    else:
+        i += 1
+        prev = lst[i]
+print("".join(lst))
