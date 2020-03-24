82378f07517705e91eadc63146396ddcf7474e0b
Tetiana Korchak <korchak.tetiana@gmail.com>
2020-03-23 21:25:34 -0500

    Refactoring

--- scripting/replace_string_in_file.py
+++ scripting/replace_string_in_file.py
@@ -1,13 +1,26 @@
-
+import json
 
 
 my_file = open('logs.jsonl', 'r')
 new_file = open('updated_logs.jsonl', 'w')
-data = my_file.readlines()
-rep = ',"_type":"log"'
-for line in data:
-    if rep in line:
-        line = line.replace(',"_type":"log"', '')
-    new_file.write(line)
+
+for line in my_file.readlines():
+    try:
+        line = json.loads(line)
+    except:
+        continue
+    if "index" in line:
+        continue
+    if "coordinates" in line["geo"]:
+        if line["geo"]["coordinates"]["lat"] > 36:
+            new_file.write(json.dumps(line))
+
+# new_file = open('updated_logs.jsonl', 'w')
+# data = my_file.readlines()
+# rep = ',"_type":"log"'
+# for line in data:
+#     if rep in line:
+#         line = line.replace(',"_type":"log"', '')
+#     new_file.write(line)
 my_file.close()
 new_file.close()
